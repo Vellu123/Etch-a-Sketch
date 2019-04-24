bf51f4afd8ad7ea4816721d8cd8fb35c01ca86fa
Vellu <makinen.vellu@gmail.com>
2019-04-24 14:15:42 +0300

    Add event listeners to gridbox

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ for (row=0;row<=15;row++){
 
     for (column=0;column<= 15;column++){
         let box = document.createElement("div");
-        box.className = "gridBox";
+        box.id = "gridBox";
         containerDiv.appendChild(box);
 
     }
 }
+
+function boxColor(){
+    console.log(this);
+    document.getElementById("gridBox").classList.add("coloredBox");
+    return;
+}
+
+document.getElementById("gridBox").addEventListener("mouseover", boxColor);
